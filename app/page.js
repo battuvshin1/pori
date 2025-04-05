42e9f91ae48d1407d43866353076ab62d0778fe5
battuvshin0 <atmospherec8@gmail.com>
2025-04-05 18:36:50 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 export default function Home() {
   return (
     <div className="m-6 flex flex-col gap-6">
